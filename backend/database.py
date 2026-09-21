cb522b276d9977470d5378d4adf4e49ffb8a0bd5
Boluwatife <bolutifelevi@gmail.com>
2026-09-21 13:16:42 +0100

    Update database.py to use DATABASE_URL env var for Neon

--- backend/database.py
+++ backend/database.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./chat.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
